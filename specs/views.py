5d6b887d9516f0b6029405bc12df7ab4c41105c7
Dima Lol <zonex1995@gmail.com>
2021-09-04 23:14:50 +0300

    Added new form for new feature

--- specs/views.py
+++ specs/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
-from .forms import NewCategoryForm
+from .forms import NewCategoryForm, CategoryFeature, NewCategoryFeatureKeyForm
 
+@method_decorator(login_required, name='dispatch')
 class BaseSpecView(View):
 
     def get(self, request, *args, **kwargs):
@@ -24,3 +27,22 @@ class NewCategoryView(View):
             form.save()
             return HttpResponseRedirect('/product-features/')
         return render(request, 'new_category.html', context)
+
+
+class CreateNewFeature(View):
+
+    def get(self, request, *args, **kwargs):
+        form = NewCategoryFeatureKeyForm(request.POST or None)
+        context = {'form': form}
+        return render(request, 'new_feature.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form = NewCategoryFeatureKeyForm(request.POST or None)
+        if form.is_valid():
+            new_category_feature_key = form.save(commit=False)
+            new_category_feature_key.category = form.cleaned_data['category']
+            new_category_feature_key.feature_name = form.cleaned_data['feature_name']
+            new_category_feature_key.save()
+            return HttpResponseRedirect('/product-features/')
+        context = {'form': form}
+        return render(request, 'new_feature.html', context)
