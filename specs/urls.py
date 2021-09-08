8eb578ece5e38c88039054f81341ec6ef0b84354
Dima Lol <zonex1995@gmail.com>
2021-09-08 22:24:52 +0300

    Completely created form /product-features/

--- specs/urls.py
+++ specs/urls.py
@@ -10,7 +10,11 @@ from .views import (
     NewProductFeatureView,
     SearchProductAjaxView,
     AttachNewFeatureToProduct,
-    ProductFeatureChoicesAjaxView
+    ProductFeatureChoicesAjaxView,
+    CreatedNewProductFeatureAjaxView,
+    UpdateProductFeaturesView,
+    ShowProductFeaturesForUpdate,
+    UpdateProductFeaturesAjaxView
 )
 
 urlpatterns = [
@@ -21,8 +25,12 @@ urlpatterns = [
     path('feature-choice/', FeatureChoiceView.as_view(), name='feature-choice-validators'),
     path('feature-create/', CreateFeatureView.as_view(), name='feature-create'),
     path('new-product-feature/', NewProductFeatureView.as_view(), name='new-product-feature'),
-    path('search-product', SearchProductAjaxView.as_view(), name='search-product'),
-    path('attach-feature', AttachNewFeatureToProduct.as_view(), name='attach-feature'),
-    path('product-feature/', ProductFeatureChoicesAjaxView.as_view(), name='product-feature')
+    path('search-product/', SearchProductAjaxView.as_view(), name='search-product'),
+    path('attach-feature/', AttachNewFeatureToProduct.as_view(), name='attach-feature'),
+    path('product-feature/', ProductFeatureChoicesAjaxView.as_view(), name='product-feature'),
+    path('attach-new-product-feature/', CreatedNewProductFeatureAjaxView.as_view(), name='attach-new-product-feature'),
+    path('update-product-features/', UpdateProductFeaturesView.as_view(), name='update-product-features'),
+    path('show-product-features-for-update/', ShowProductFeaturesForUpdate.as_view(), name='show-product-features-for-update'),
+    path('update-product-features-ajax/', UpdateProductFeaturesAjaxView.as_view(), name='update-product-features-ajax')
 ]
 
